6f05c4d536c6513fc1ca250918308fe5cf648dd1
Gonzalo Cabezas Núñez <dev@sandokan.cat>
2026-01-25 05:16:30 +0100

    fix path error

---
+++ public/js/index.js
@@ -0,0 +1,4 @@
+// EXPORTS TO USE LIKE A SUBMODULE
+export { default as logger } from './logger.js';
+export { validateJSON } from './validateJSON.js';
+export { validateCarousel } from './validateCarousel.js';
